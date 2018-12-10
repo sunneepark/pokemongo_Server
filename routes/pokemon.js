f7991b91d19e3dcb465516098b71391cf64646c1
MinJun Kweon <Minz0000000@gmail.com>
2018-12-10 20:45:14 +0900

    포켓몬 포획 API 따기 & 주변 포켓몬 가져오기 구현

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,9 +5,25 @@ var pokemon = require('../controller/pokemon');
 
 router.get('/', function (req, res, next) {
     pokemon.allPokemons(function (err, pokemons) {
-        if (err) return res.send(err);
+        if (err) return res.status(500).send(err);
         return res.send(pokemons);
-    })
-})
+    });
+});
+
+/* 포켓몬 포획 */
+router.get('/catch', function (req, res, next) {
+    pokemon.catch(function (err, pokemons) {
+        if (err) return res.status(500).send(err);
+        return res.send({ 'code' : 200, 'message' : '성공적으로 포켓몬을 잡았습니다.' });
+    });
+});
+
+/* 주변 포켓몬 목록 */
+router.get('/nearby', function (req, res, next) {
+    pokemon.nearbyPokemons(function (err, pokemons) {
+        if (err) return res.status(500).send(err);
+        return res.send(pokemons);
+    });
+});
 
 module.exports = router;
